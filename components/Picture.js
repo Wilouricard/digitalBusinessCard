2d18247bb480c646713fd54c0be7d1efd4df19ed
Nam Tang <nam.tang@live.fr>
2022-09-18 11:03:17 +0200

    second

--- components/Picture.js
+++ components/Picture.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Picture(){
     return (
-        <>
+        <div>
         <img src="../ReadyPlayerMe-Avatar.png" className="profile-picture"/>
-        </>
+        </div>
     )
 }
